f8ce2b82b9bb48d164423c623e74449f6a19701f
meryeme <meriembouzaid31@gmail.com>
2025-07-16 11:27:31 +0100

    Initial commit

---
+++ src/test/java/com/dira/diravenir1/Diravenir1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dira.diravenir1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Diravenir1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
